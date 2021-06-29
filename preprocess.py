f02c89b2f185bded2cccd410110a73d2ea105419
VectorVP <vvtumko@mail.ru>
2021-06-29 19:45:32 +0300

    Remove extra ipynb lines

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 from PIL import Image
 import numpy as np
@@ -13,17 +7,13 @@ from os import listdir
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-
-# In[3]:
-
-
 def analyse_images(path_to_images):
     image_files = [f for f in listdir(path_to_images) if isfile(join(path_to_images, f))]
     df = pd.DataFrame(columns=['file_name', 'channel', 'weigth', 'heigth'])
     count = 0
     for file in image_files:
         img = Image.open(join(path_to_images, file))
-        np_shape = np.asarray(img).shape    
+        np_shape = np.asarray(img).shape
         if len(np_shape)==2:
             df.loc[count,:] = [file] + [1, np_shape[0], np_shape[1]]
         else:
@@ -32,36 +22,10 @@ def analyse_images(path_to_images):
     df = df.astype({"channel": int,"weigth": int, "heigth": int})
     return df
 
-
-# In[6]:
-
-
 # looks for orignal images
 path_to_images = 'C:\\Users\\demko_r\\ML\\Electiv_CV\\images2\\'
 df = analyse_images(path_to_images)
 df[(df.weigth < 200) | (df.heigth < 200)]
-
-
-# In[7]:
-
-
-df.hist('channel')
-
-
-# In[8]:
-
-
-df.hist('weigth')
-
-
-# In[9]:
-
-
-df.hist('heigth')
-
-
-# In[11]:
-
 
 def rename_images(path_to_images, index):
     image_files = [f for f in listdir(path_to_images) if isfile(join(path_to_images, f))]
@@ -73,7 +37,7 @@ def rename_images(path_to_images, index):
 
 def image_pred_proccesing(path_to_images,path_to_preproc_images,ext='jpg'):
     image_files = [f for f in listdir(path_to_images) if isfile(join(path_to_images, f))]
-    for file in tqdm(image_files,total=len(image_files)): 
+    for file in tqdm(image_files,total=len(image_files)):
         pil_img = Image.open(join(path_to_images, file)).convert('RGB')
         img_resize = pil_img.resize((500,500))
         # np_arr = np.asarray(pil_img)
@@ -91,18 +55,12 @@ def image_pred_proccesing(path_to_images,path_to_preproc_images,ext='jpg'):
         #     # if channel more than 3, left only firs 3 channels
         #     np_arr = np_arr[:,:,0:3]
         # new_img = Image.fromarray(np_arr,mode = 'RGB')
-        
+
         file, _ = os.path.splitext(file)
         img_resize.save(f'{path_to_preproc_images}{file}.{ext}')
-        
+
 path_to_images = 'C:\\Users\\demko_r\\ML\\Electiv_CV\\images2\\'
 path_to_preproc_images = 'C:\\Users\\demko_r\\ML\\Electiv_CV\\resize_images\\'
 rename_images(path_to_images, index = 200)
 image_pred_proccesing(path_to_images,path_to_preproc_images)
-
-
-# In[3]:
-
-
-
 
